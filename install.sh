a758b7d361612883e88008eb4ef48276cc333d19
Maciej Marczynski <marczyns@jagular.iisg.agh.edu.pl>
2013-10-16 09:12:47 +0200

    Installator fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#(mkdir -p ~/.vim/bundle;
-#git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim)
+(mkdir -p ~/.vim/bundle;
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim)
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
