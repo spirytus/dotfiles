17379e5d5ab91f2c144ad53380401355cd5010e4
Maciej Marczynski <maciek@MacBook-Pro-Maciej.local>
2014-07-09 08:18:49 +0200

    Install Neobundle

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 export TERM=xterm-256color
 #(mkdir -p ~/.vim/bundle;
 #git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim)
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
